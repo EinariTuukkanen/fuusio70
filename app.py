b0ed40ead5ffc517e60e27da6f3d14516113b234
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2018-01-29 23:44:39 +0200

    Debugging

--- app.py
+++ app.py
@@ -41,6 +41,20 @@ mail = Mail(app)
 # >>> REST API ENDPOINTS
 # ======================================
 
+from datetime import datetime
+
+
+@app.route('/users', methods=['GET'])
+@cross_origin(origins='*')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
 
 @app.route('/users', methods=['GET'])
 @cross_origin(origins='*')
@@ -139,4 +153,5 @@ def session_timeout(mongo_db, user_id):
 # ======================================
 
 
-app.run(port=5005)
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // >> CONFIG
 // ============================
 
-var API_BASE_URL = 'http://localhost:5005'; // 138.68.91.244:5000
+var API_BASE_URL = 'http://localhost:5000'; // 138.68.91.244:5000
 var SESSION_TIMEOUT = 1800 // seconds
